ee3451458f3076ebaffeb553a4a23d080551578a
Mark Schmele <MSchmele@gmail.com>
2023-12-04 10:46:46 -0700

    Solve Part 1

--- 01/pt1.exs
+++ 01/pt1.exs
@@ -18,13 +18,34 @@ defmodule Trebuchet do
   # .'__/____/___________________`._____` .'.'
   # |____________________________________|.' LGB
 
-  def parse_input(path) do
-    {:ok, contents} = File.read(path)
+  def remove_letters(lines) do
+    Enum.map(
+      lines,
+      fn line ->
+        num_string = String.replace(line, ~r/[^\d]/, "")
+        num_size = String.length(num_string)
+        first_char = String.first(num_string)
+        last_char = String.last(num_string)
 
-    String.split(contents, "\n", trim: true)
+        cond do
+          num_size >= 1 ->
+            [first_char, last_char]
+            |> Enum.join()
+            |> String.to_integer()
+
+          num_size < 1 ->
+            nil
+        end
+      end
+    )
   end
 
-  def remove_letters(input) do
+  def parse_input(path) do
+    path
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> remove_letters
+    |> Enum.sum()
   end
 end
 
