9e0c6cbd99562145ca451282f321f0862458669a
Mark Schmele <MSchmele@gmail.com>
2023-12-11 15:00:17 -0700

    finally got around to doing day 2

--- 02/pt1.exs
+++ 02/pt1.exs
@@ -2,16 +2,16 @@ defmodule Cubes do
   def parse_single_game(game) do
     game
     |> String.split(",", trim: true)
-    |> Enum.map(fn x ->
-      x |> String.split() |> Enum.reverse() |> List.to_tuple()
-    end)
-    |> List.flatten()
+    |> Enum.map(fn x -> String.split(x) end)
+    |> Enum.into([], fn [left, right] -> %{num: String.to_integer(left), color: right} end)
   end
 
   def get_max_color(results, color) do
-    filtered_results = Enum.filter(results, fn result -> result.key == color end)
-
     results
+    |> List.flatten()
+    |> Enum.filter(fn result -> Map.get(result, :color) == color end)
+    |> Enum.map(fn x -> Map.get(x, :num) end)
+    |> Enum.max()
   end
 
   def input_line_to_struct(line) do
@@ -20,11 +20,10 @@ defmodule Cubes do
     processed_results =
       results
       |> String.split(";", trim: true)
-      |> Enum.join(",")
-      |> parse_single_game()
+      |> Enum.map(fn result -> parse_single_game(result) end)
 
     %{
-      game_number: number |> String.replace(~r/[^\d]/, ""),
+      game_number: number |> String.replace(~r/[^\d]/, "") |> String.to_integer(),
       max_red: get_max_color(processed_results, "red"),
       max_green: get_max_color(processed_results, "green"),
       max_blue: get_max_color(processed_results, "blue")
@@ -36,12 +35,30 @@ defmodule Cubes do
     |> File.read!()
     |> String.split("\n", trim: true)
     |> Enum.map(fn line -> input_line_to_struct(line) end)
+    |> dbg
   end
 
   def analyze_games_from_file(file) do
+    red = 12
+    green = 13
+    blue = 14
     games = parse_games(file)
-    dbg()
+
+    possible_games =
+      games
+      |> Enum.filter(fn game ->
+        game.max_red <= red &&
+          game.max_green <= green &&
+          game.max_blue <= blue
+      end)
+
+    id_sum = Enum.reduce(possible_games, 0, fn x, acc -> x.game_number + acc end)
+
+    %{
+      possible_games: possible_games,
+      sum: id_sum
+    }
   end
 end
 
-Cubes.analyze_games_from_file("smallinput.txt")
+Cubes.analyze_games_from_file("input.txt")
